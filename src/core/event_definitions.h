fd323f9e3df156a37b5a36e195fdc87c14a6cea8
iyashj <iyashj@protonmail.ch>
2025-09-23 21:18:24 +0300

    adds MultiplayerEvent to event_definitions

--- src/core/event_definitions.h
+++ src/core/event_definitions.h
@@ -7,7 +7,16 @@ struct MenuEvent {
     enum class Type : uint8_t { StartLocalGame, StartOnlineGame, ExitGame, BackToMenu } type;
 };
 
-
-
+struct MultiplayerEvent {
+    enum class Type : uint8_t { 
+        StartServer, 
+        ConnectToServer, 
+        Disconnect, 
+        LobbyUpdate,
+        GameStart 
+    } type;
+    
+    std::string serverAddress;
+};
 
 #endif // DIDDLEDOODLEDUEL_EVENT_DEFINITIONS_H
